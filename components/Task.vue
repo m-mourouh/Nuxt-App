2f67f3dfd926a3cde6e40d24eb0669d6bc3e7824
Mohamed Mourouh <mmourouh@smb.sqli.com>
2022-11-27 22:47:37 +0100

    first nuxt app

---
+++ components/Task.vue
@@ -0,0 +1,27 @@
+<template>
+	<div :class="`bg-light task ${task.done ? 'is-complete' : ''}`">
+		<div class="content">{{ task.content }}</div>
+		<div class="buttons">
+			<button @click="toggleDone" class="btn btn-success">{{ task.done ? 'Undo' : 'Done' }}</button>
+			<button @click="removeTask" class="btn btn-danger">Delete</button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['task'],
+	methods: {
+		toggleDone() {
+			this.$store.commit('TOGGLE_TASK', this.task);
+		},
+		removeTask() {
+			this.$store.commit('REMOVE_TASK', this.task);
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <main>
+    <h1 class="mb-5">Nuxt | Todo List</h1>
+    <div class="create-new">
+      <input
+        type="text"
+        v-model="newTask"
+        placeholder="Add a new task"
+        @keypress.enter="addTask"
+        class="form-control"
+      />
+      <button @click="addTask" class="btn btn-dark ms-3">Add</button>
+    </div>
+
+    <div class="tasks">
+      <Task v-for="(task, i) in $store.state.tasks" :key="i" :task="task" />
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: "",
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim()) {
+        this.$store.commit("ADD_TASK", this.newTask);
+        this.newTask = "";
+      }
+    },
+  },
+};
+</script>
+
+
